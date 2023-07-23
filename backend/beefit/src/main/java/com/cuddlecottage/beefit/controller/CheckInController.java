9435595882dfbb6f224f61dddfa8f68b16126b6d
Alan Vo <alandatvo@yahoo.com>
2023-07-22 20:49:30 -0500

    change request mapping for checkin control

--- backend/beefit/src/main/java/com/cuddlecottage/beefit/controller/CheckInController.java
+++ backend/beefit/src/main/java/com/cuddlecottage/beefit/controller/CheckInController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/checkin/{id}")
+@RequestMapping("/api/checkin")
 public class CheckInController {
     @Autowired
     private CheckInService checkinService;
